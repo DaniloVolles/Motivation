3f923278f74911550f5fa951af66442fa128a7e7
Danilo Volles <danilo.volles@outlook.com>
2023-07-24 10:06:00 -0300

    User activity implementation

--- app/src/main/java/com/example/motivation/UserActivity.kt
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -1,11 +1,33 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.motivation.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnNewActivity.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btn_new_activity -> newActivity()
+        }
+    }
+
+    private fun newActivity(){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
 }
